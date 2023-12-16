053ea7e1a5a8533f174f1c9f6f1d924abdb299ee
Bandana Prakash <146561559+bandanaprakash@users.noreply.github.com>
2023-12-16 18:34:29 +0000

    done

--- 01.Basics/1st.js
+++ 01.Basics/1st.js
@@ -1,1 +1,15 @@
 console.log("Bandana Prakash")
+
+console.log("Bandana Prakash")
+
+const accountId = 121
+let accountEmail= "gopi@gmail.com"
+var accountPassword="23847"
+accountCity="Jaipur"
+
+accountEmail = "happy@gmail.com"
+accountPassword="129812"
+accountCity="Bengaluru"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);

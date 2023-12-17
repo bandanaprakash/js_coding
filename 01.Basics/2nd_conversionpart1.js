f60073dd56b9cbdfbd53ea31a11c707fdb2b6254
Bandana Prakash <146561559+bandanaprakash@users.noreply.github.com>
2023-12-17 16:31:21 +0000

    conversion done

---
+++ 01.Basics/2nd_conversionpart1.js
@@ -0,0 +1,137 @@
+/* 
+let score = 99
+console.log(score);
+console.log(typeof score);     //number
+console.log(typeof(score));
+*/
+/*
+let score = "99"
+console.log(score);
+console.log(typeof score);        //string
+console.log(typeof(score));
+*/
+/*
+let score = ""
+console.log(score);
+console.log(typeof score);               //string
+console.log(typeof(score));
+*/
+/*
+let score = null
+console.log(score);
+console.log(typeof score);           //object
+console.log(typeof(score));
+*/
+/*
+let score = undefined
+console.log(score);
+console.log(typeof score);       //undefined
+console.log(typeof(score));
+*/
+
+// let score = 99
+// console.log(score);
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber= Number(score)
+// console.log(valueInNumber);
+
+// let score = "99"
+// console.log(score);
+// console.log(typeof score);     //string
+// console.log(typeof(score));
+
+// let valueInNumber= Number(score)     //99
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score = "99abc"
+// console.log(score);
+// console.log(typeof score);        //string
+// console.log(typeof(score));
+
+// let valueInNumber= Number(score)       NaN
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score = ""
+// console.log(score);
+// console.log(typeof score);          //string
+// console.log(typeof(score));
+
+// let valueInNumber= Number(score)           //0
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score = null
+// console.log(score);
+// console.log(typeof score);
+// console.log(typeof(score));          //object
+
+// let valueInNumber= Number(score)           //0
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score = undefined
+// console.log(score);
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber= Number(score)          //NaN
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score = "Bandana"
+// console.log(score);
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber= Number(score)       //NaN
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score = true
+// console.log(score);
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber= Number(score)          //1
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score = 1
+// console.log(score);
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInBoolean= Boolean(score)      //true
+// console.log(typeof valueInBoolean);
+// console.log(valueInBoolean);
+
+// let score = "Bandana"
+// console.log(score);
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInBoolean= Boolean(score)   //true
+// console.log(typeof valueInBoolean);
+// console.log(valueInBoolean);
+
+// let score = ""
+// console.log(score);
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInBoolean= Boolean(score)      //false
+// console.log(typeof valueInBoolean);
+// console.log(valueInBoolean);
+
+let score = 33
+console.log(score);
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInString= String(score)      //33
+console.log(typeof valueInString);
+console.log(valueInString);

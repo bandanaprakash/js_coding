32f65e70deca4831ce0d6d303c9dbb60858ff2d8
Bandana Prakash <146561559+bandanaprakash@users.noreply.github.com>
2023-12-17 17:43:51 +0000

    operation done

---
+++ 01.Basics/3rd_operation.js
@@ -0,0 +1,31 @@
+/*     Operation      */
+
+let value= 12
+let negValue= -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1="Hello"
+// let str2=" World"
+// console.log(`${str1} ${str2}`);    // Hello  World
+// let str3= str1 + str2
+// console.log(str3);                 // Hello  World
+
+// console.log(1 + 2 + 2);            //5
+// console.log("1" + 2 + 2);         //122
+// console.log(1 + "2" + 2);         //122
+// console.log(1 + 2 + "2");         //32
+
+console.log(2>1);
+console.log(2>=1);
+console.log(1<2);
+console.log(2<=3);
+console.log(2!=2);
+console.log(2==2);
+console.log(2===2);                //true  

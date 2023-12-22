ddcfb101b61745771f2694f16a06504c59eff53c
Bandana Prakash <146561559+bandanaprakash@users.noreply.github.com>
2023-12-22 00:55:52 +0000

    done

---
+++ 01.Basics/4th_String.js
@@ -0,0 +1,105 @@
+/***************************** */
+
+/*       Strings          */
+  
+const name = "Bandana"
+const repoCount = 50
+
+//console.log(name + repoCount + "value");
+
+//console.log(`My Name is ${name} and my repo count is ${repoCount}`);
+
+//object using string
+const gameName = new String('b_Pro') 
+// console.log(gameName);
+// console.log(gameName[3]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('o'));
+
+// const newstring= gameName.substring(0,3)
+// console.log(newstring);
+
+// const anotherString = gameName.slice(0,3)
+// console.log(anotherString);
+// const anotherString1 = gameName.slice(-4,4)
+// console.log(anotherString1);
+
+// const newStringOne = "     Bandana     "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url ="https//bandana.com/bandanaprakash2000"
+// console.log(url.replace('2000','22347'));
+// console.log(url.includes('bandana'));
+
+const newValue= new String("Ban-d-ana")
+console.log(newValue.split('-'));
+
+/*
+String {'b_pro'}
+0: "b"
+1: "_"
+2: "p"
+3: "r"
+4: "o"
+length: 5
+[[Prototype]]
+: 
+String 
+anchor: ƒ anchor()
+at: ƒ at()
+big: ƒ big()
+blink: ƒ blink()
+bold: ƒ bold()
+charAt: ƒ charAt()
+charCodeAt: ƒ charCodeAt()
+codePointAt: ƒ codePointAt()
+concat: ƒ concat()
+constructor: ƒ String()
+endsWith: ƒ endsWith()
+fixed: ƒ fixed()
+fontcolor: ƒ fontcolor()
+fontsize: ƒ fontsize()
+includes: ƒ includes()
+indexOf: ƒ indexOf()
+isWellFormed: ƒ isWellFormed()
+italics: ƒ italics()
+lastIndexOf: ƒ lastIndexOf()
+length: 0
+link: ƒ link()
+localeCompare: ƒ localeCompare()
+match: ƒ match()
+matchAll: ƒ matchAll()
+normalize: ƒ normalize()
+padEnd: ƒ padEnd()
+padStart: ƒ padStart()
+repeat: ƒ repeat()
+replace: ƒ replace()
+replaceAll: ƒ replaceAll()
+search: ƒ search()
+slice: ƒ slice()
+small: ƒ small()
+split: ƒ split()
+startsWith: ƒ startsWith()
+strike: ƒ strike()
+sub: ƒ sub()
+substr: ƒ substr()
+substring: ƒ substring()
+sup: ƒ sup()
+toLocaleLowerCase: ƒ toLocaleLowerCase()
+toLocaleUpperCase: ƒ toLocaleUpperCase()
+toLowerCase: ƒ toLowerCase()
+toString: ƒ toString()
+toUpperCase: ƒ toUpperCase()
+toWellFormed: ƒ toWellFormed()
+trim: ƒ trim()
+trimEnd: ƒ trimEnd()
+trimLeft: ƒ trimStart()
+trimRight: ƒ trimEnd()
+trimStart: ƒ trimStart()
+valueOf: ƒ valueOf()
+Symbol(Symbol.iterator): ƒ [Symbol.iterator]() [[Prototype]]: Object
+*/
